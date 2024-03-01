f127d5ce3c043e39a6fb2841de64f2f051eda1f7
lucasdaher <contato@lucasdaher.com>
2024-03-01 05:11:53 -0300

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="">Header</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/error.tsx
@@ -0,0 +1,7 @@
+const Error = () => {
+  return (
+    <div className="text-3xl text-blue-500">Um erro foi encontrado meu cpc</div>
+  );
+};
+
+export default Error;
